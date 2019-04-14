46a6c0ce6441b299f1fa8c1b992c50cdc1ede445
Marek Bastl <marek.bastl5@gmail.com>
2019-04-14 13:42:20 +0200

    Database

--- elektroDiar/MainWindow.xaml.cs
+++ elektroDiar/MainWindow.xaml.cs
@@ -21,51 +21,159 @@ namespace elektroDiar
     /// </summary>
     public partial class MainWindow : Window
     {
-        public Note note = new Note();
 
         public MainWindow()
         {
             InitializeComponent();
+            getNotes();
+            
         }
 
+        private void getNotes()
+        {
+            Notes.Items.Clear();
+            var Items = App.NoteDatabase.getNotes().Result;
+            foreach (var item in Items)
+            {
+                Notes.Items.Add(item);
+            }
+        }
+        public int id;
         private void Notes_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            if (editMode)
+            {
+                dynamic selectedItem = Notes.SelectedItems[0];
+                nameSave.Text = selectedItem.name;
+                lastNameSave.Text = selectedItem.lastName;
+                ageSave.Text = selectedItem.age;
+                emailSave.Text = selectedItem.email;
+                phoneSave.Text = selectedItem.phone;
+                dateP.Text = selectedItem.date;
+                noteSave.Text = selectedItem.note;
+                id = selectedItem.id_note;
+                Save.Content = "EDIT";
+                Save.IsEnabled = true;
+            }
+            else
+            {
+                if (Notes.SelectedItems.Count != 0)
+                {
+                    dynamic selectedItem = Notes.SelectedItems[0];
+                    App.NoteDatabase.deleteById(selectedItem.id_note);
+                    getNotes();
+                }
+            }
         }
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
-            if(nameSave.Text != "" & lastNameSave.Text != "" & ageSave.Text != "" & emailSave.Text != "" & dateP.Text != "" & isNumeric(ageSave.Text) & phoneSave.Text != "")
+            if (editMode)
             {
-                ErrorL.Content = "";
-                note.name = nameSave.Text;
-                note.lastName = lastNameSave.Text;
-                note.age = ageSave.Text;
-                note.email = emailSave.Text;
-                note.phone = phoneSave.Text;
-                note.date = dateP.Text;
-                if(radB1.IsChecked == true)
+                if (nameSave.Text != "" & noteSave.Text != "" & dateP.Text != "" & isNumeric(ageSave.Text))
                 {
-                    note.sex = radB1.Content.ToString();
-
+                    editMode = false;
+                    Save.Content = "SAVE";
+                    Save.IsEnabled = true;
+                    Note note = new Note();
+                    ErrorL.Content = "";
+                    note.id_note = id;
+                    note.name = nameSave.Text;
+                    note.lastName = lastNameSave.Text;
+                    note.age = ageSave.Text;
+                    note.email = emailSave.Text;
+                    note.phone = phoneSave.Text;
+                    note.date = dateP.Text;
+                    note.note = noteSave.Text;
+                    if (radB1.IsChecked == true)
+                    {
+                        note.sex = radB1.Content.ToString();
+                    }
+                    else
+                    {
+                        note.sex = radB2.Content.ToString();
+                    }
+                    App.NoteDatabase.UpdateItemAsync(note);
                 }
                 else
                 {
-                    note.sex = radB2.Content.ToString();
-
+                    ErrorL.Content = "Zadej všechny údaje";
                 }
-                App.NoteDatabase.SaveItemAsync(note);
             }
             else
             {
-                ErrorL.Content = "Zadej všechny údaje";
+                if (nameSave.Text != "" & noteSave.Text != "" & dateP.Text != "" & isNumeric(ageSave.Text))
+                {
+                    Note note = new Note();
+                    ErrorL.Content = "";
+                    note.name = nameSave.Text;
+                    note.lastName = lastNameSave.Text;
+                    note.age = ageSave.Text;
+                    note.email = emailSave.Text;
+                    note.phone = phoneSave.Text;
+                    note.date = dateP.Text;
+                    note.note = noteSave.Text;
+                    if (radB1.IsChecked == true)
+                    {
+                        note.sex = radB1.Content.ToString();
+                    }
+                    else
+                    {
+                        note.sex = radB2.Content.ToString();
+                    }
+                    App.NoteDatabase.SaveItemAsync(note);
+                }
+                else
+                {
+                    ErrorL.Content = "Zadej všechny údaje";
+                }
             }
-            
+            getNotes();
         }
         private static bool isNumeric(string number)
         {
             int isNumber;
             return int.TryParse(number, out isNumber);
+        }
+
+        private void SearchB_Click(object sender, RoutedEventArgs e)
+        {
+
+            Notes.Items.Clear();
+            var Items = App.NoteDatabase.getNotesByNameAndMail(searchEmail.Text, searchName.Text).Result;
+            foreach (var item in Items)
+            {
+                Notes.Items.Add(item);
+            }
+        }
+
+        private void DeleteB_Click(object sender, RoutedEventArgs e)
+        {
+            App.NoteDatabase.DeleteAll();
+            getNotes();
+        }
+        public bool editMode = false;
+        private void EditB_Click(object sender, RoutedEventArgs e)
+        {
+            if (editMode)
+            {
+                EditB.Content = "EDIT MODE";
+                editMode = false;
+                Save.IsEnabled = true;
+                Save.Content = "SAVE";
+            }
+            else
+            {
+                EditB.Content = "DELETE MODE";
+                editMode = true;
+                Save.IsEnabled = false;
+                Save.Content = "EDIT";
+            }
+        }
+
+        private void RefreshB_Click(object sender, RoutedEventArgs e)
+        {
+            getNotes();
         }
     }
 }
--- elektroDiar/classes/noteDatabase.cs
+++ elektroDiar/classes/noteDatabase.cs
@@ -18,27 +18,39 @@ namespace elektroDiar.classes
         }
         public Task<List<Note>> QueryCustomExist(string name)
         {
-            return database.QueryAsync<Note>("select ID FROM [Item] where Name ='" + name + "'");
+            return database.QueryAsync<Note>("select ID FROM [Note] where Name ='" + name + "'");
         }
         public Task<List<Note>> SelectByName(string name)
         {
-            return database.QueryAsync<Note>("select * FROM [Item] where name  LIKE '%" + name + "%'");
+            return database.QueryAsync<Note>("select * FROM [Note] where name  LIKE '%" + name + "%'");
         }
         public Task<List<Note>> QueryCustom()
         {
-            return database.QueryAsync<Note>("select * FROM [Item]");
+            return database.QueryAsync<Note>("select * FROM [Note]");
         }
         public Task<List<Note>> Add()
         {
-            return database.QueryAsync<Note>("INSERT INTO [Item] (Name,Text) VALUES (`Ahoj`,`Pepa`)");
+            return database.QueryAsync<Note>("INSERT INTO [Note] (Name,Text) VALUES (`Ahoj`,`Pepa`)");
         }
         public Task<List<Note>> DeleteAll()
         {
-            return database.QueryAsync<Note>("Delete FROM [Item]");
+            return database.QueryAsync<Note>("Delete FROM [Note]");
         }
-        public Task<List<Note>> ReturnPrices()
+        public Task<List<Note>> deleteById(int id)
         {
-            return database.QueryAsync<Note>("SELECT price FROM [Item]");
+            return database.QueryAsync<Note>("Delete FROM [Note] where id_note ='" + id + "'");
+        }
+        public Task<List<Note>> getById(int id)
+        {
+            return database.QueryAsync<Note>("select * FROM [Note] where id_note ='" + id + "'");
+        }
+        public Task<List<Note>> getNotes()
+        {
+            return database.QueryAsync<Note>("select * FROM [Note]");
+        }
+        public Task<List<Note>> getNotesByNameAndMail(string mail,string name)
+        {
+            return database.QueryAsync<Note>("select * FROM [Note] where name  LIKE '%" + name + "%' OR email LIKE '%" + mail + "%'");
         }
         // Query
         public Task<List<Note>> GetItemsAsync()
@@ -49,7 +61,7 @@ namespace elektroDiar.classes
         // Query using SQL query string
         public Task<List<Note>> GetItemsNotDoneAsync()
         {
-            return database.QueryAsync<Note>("SELECT * FROM [Item] WHERE [Done] = 0");
+            return database.QueryAsync<Note>("SELECT * FROM [Note] WHERE [Done] = 0");
         }
 
         // Query using LINQ
@@ -58,6 +70,10 @@ namespace elektroDiar.classes
         public Task<int> SaveItemAsync(Note item)
         {
             return database.InsertAsync(item);
+        }
+        public Task<int> UpdateItemAsync(Note item)
+        {
+            return database.UpdateAsync(item);
         }
         public Task<int> SaveList(List<Note> items)
         {
